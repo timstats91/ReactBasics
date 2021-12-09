2bda4b7a4614c3abdc59ce21037ab22fbfbd6394
timstats91 <timstats91@gmail.com>
2021-12-09 12:43:11 -0600

    Using JSX

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 
-// This new element named 'Title' below:
-const title = React.createElement(
-    'h1',
-    { id: 'main-title', title: 'This is a title.' },
-    'My first React Element!' 
-);
-// Creates this:
-// <h1 id="main-title" title="This is a title">My first React Element!</h1>
+const name = 'Tim';
+const desc = 'I just learned how to create a React node and render it into the DOM.';
+const myTitleID = 'main-title';
 
-const desc = React.createElement(
-    'p',
-    null,
-    'I just learned how to create a React node and render it into the DOM.'
-);
-
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
+const header = (
+    <header>
+        <h1 id={myTitleID}>{ name }'s first React Element!</h1>
+        <p>{ desc }</p>
+    </header>
 );
 
 ReactDOM.render(
